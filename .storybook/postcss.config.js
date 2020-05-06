1adda700d9405d7bef3224c957d0c3d886cf96a1
ShinCurry <shincurryyang@gmail.com>
2020-05-06 12:39:38 +0800

    fix storybook build

--- .storybook/postcss.config.js
+++ .storybook/postcss.config.js
@@ -1,23 +1,9 @@
 const path = require('path')
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [
-    '../stories/**/*.jsx',
-    '../stories/**/*.tsx',
-    '../stories/**/*.ts',
-    '../stories/**/*.mdx',
-  ],
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-})
 
 module.exports = {
   plugins: [
     require('autoprefixer'),
     require('postcss-import'),
     require('tailwindcss')(path.resolve(__dirname, 'tailwind.config.js')),
-    ...(
-      process.env.NODE_ENV === 'production'
-      ? [purgecss]
-      : []
-    ),
   ]
 }
