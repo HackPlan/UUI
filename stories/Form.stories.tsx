10c87dabf2341cf62332fa83b60057c4e8124e56
ShinCurry <shincurryyang@gmail.com>
2020-05-26 15:55:07 +0800

    Form - Controlled Mode

---
+++ stories/Form.stories.tsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import { TextField, TextArea, RadioGroup, Radio, Checkbox, DatePicker, Button, HTMLSelect } from '../src';
+
+export default {
+  title: 'Form',
+};
+
+const LabeledControl = (props) => {
+  return (
+    <div className="flex row mb-4">
+      {props.children}
+    </div>
+  )
+}
+const Label = (props) => {
+  return (
+    <label className="flex justify-end items-center mr-2 w-24 h-8">
+      {props.children}
+    </label>
+  )
+}
+
+export const FormControlledMode = () => {
+  const [name, setName] = useState('')
+  const [introduction, setIntroduction] = useState('')
+  const [gender, setGender] = useState('male')
+  const [birthday, setBirthday] = useState(null)
+  const [school, setSchool] = useState('Peking University')
+  const [skillsWeb, setSkillsWeb] = useState(false)
+  const [skillsMobile, setSkillsMobile] = useState(false)
+  const [skillsServer, setSkillsServer] = useState(false)
+  const [skillsAI, setSkillsAI] = useState(false)
+
+  return (
+    <div>
+      <LabeledControl>
+        <Label>名字</Label>
+        <TextField className="w-64" value={name} onChange={(value) => setName(value)} />
+      </LabeledControl>
+      <LabeledControl>
+        <Label>介绍</Label>
+        <TextArea className="w-64" value={introduction} onChange={(value) => setIntroduction(value)} />
+      </LabeledControl>
+      <LabeledControl>
+        <Label>性别</Label>
+        <RadioGroup name="gender" value={gender} onChange={(value) => { setGender(value) }}>
+          <Radio label={'男'} value={'male'} className={"mr-2"}></Radio>
+          <Radio label={'女'} value={'female'} className={"mr-2"}></Radio>
+          <Radio label={'其他'} value={'other'} className={"mr-2"}></Radio>
+        </RadioGroup>
+      </LabeledControl>
+      <LabeledControl>
+        <Label>生日</Label>
+        <DatePicker className="w-64" value={birthday} onChange={(value) => setBirthday(value)} />
+      </LabeledControl>
+      <LabeledControl>
+        <Label>学校</Label>
+        <HTMLSelect
+          className="w-64"
+          options={[
+            { label: 'Peking University', value: 'Peking University' },
+            { label: 'Chongqing University', value: 'Chongqing University' },
+          ]}
+          value={school}
+          onChange={(value) => setSchool(value)}
+        />
+      </LabeledControl>
+      <LabeledControl>
+        <Label>技能</Label>
+        <div className="flex flex-col">
+          <Checkbox
+            label={'Web'}
+            value={skillsWeb}
+            onChange={(value) => { setSkillsWeb(value) }}
+          />
+          <Checkbox
+            label={'Mobile'}
+            value={skillsMobile}
+            onChange={(value) => { setSkillsMobile(value) }}
+          />
+          <Checkbox
+            label={'Server'}
+            value={skillsServer}
+            onChange={(value) => { setSkillsServer(value) }}
+          />
+          <Checkbox
+            label={'AI'}
+            value={skillsAI}
+            onChange={(value) => { setSkillsAI(value) }}
+          />
+        </div>
+      </LabeledControl>
+      <LabeledControl>
+        <Label></Label>
+        <Button>提交</Button>
+      </LabeledControl>
+    </div>
+  )
+}
+
+FormControlledMode.story = {
+  name: 'Form (Controlled Mode)',
+};
