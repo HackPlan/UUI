370d8d4b222d44f8f69e1cd8de52acd2c3060fc5
ShinCurry <shincurryyang@gmail.com>
2020-05-28 15:58:06 +0800

    SegmentControl new style

--- src/components/SegmentControl/SegmentControl.tsx
+++ src/components/SegmentControl/SegmentControl.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { UUI, UUIComponentProps } from '../../core/uui';
-import { Button as UUIButton } from '../Button';
 import classNames from 'classnames';
 
 export interface SegmentControlOption<T extends string | number> {
@@ -27,32 +26,43 @@ export interface BaseSegmentControlProps<T extends string | number> {
 
 const SegmentControlNodes = {
   Root: 'div',
-  Button: UUIButton,
+  Container: 'div',
+  Option: 'div',
+  Thumb: 'div',
 } as const
 
 const BaseSegmentControl = UUI.FunctionComponent({
   name: 'SegmentControl',
   nodes: SegmentControlNodes
 }, (props: BaseSegmentControlProps<any>, nodes) => {
-  const { Root, Button } = nodes;
+  const { Root, Container, Option, Thumb } = nodes;
+
+  const selectedIndex = props.options.findIndex((i) => i.value === props.value)
+  const thumbStyle = {
+    marginLeft: `${selectedIndex / props.options.length * 100}%`,
+    width: `${1/props.options.length * 100}%`,
+  }
 
   return (
     <Root>
-      {props.options.map((option) => {
-        return (
-          <Button
-            key={option.value}
-            className={classNames({
-              'Active': props.value === option.value
-            })}
-            onClick={() => {
-              if (props.value !== option.value) {
-                props.onChange(option.value)
-              }
-            }}
-          >{option.label}</Button>
-        )
-      })}
+      <Container>
+        <Thumb style={thumbStyle} />
+        {props.options.map((option) => {
+          return (
+            <Option
+              key={option.value}
+              className={classNames({
+                'Active': props.value === option.value
+              })}
+              onClick={() => {
+                if (props.value !== option.value) {
+                  props.onChange(option.value)
+                }
+              }}
+            >{option.label}</Option>
+          )
+        })}
+      </Container>
     </Root>
   )
 })
