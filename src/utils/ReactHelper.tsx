f61167f7bf895900e81235eabb0917c8681d5d01
ShinCurry <shincurryyang@gmail.com>
2020-07-13 15:07:49 +0800

    fix ReactHelper.join unique key issue

--- src/utils/ReactHelper.tsx
+++ src/utils/ReactHelper.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 const join = (nodes: React.ReactNode[], separator: React.ReactNode) => {
   return nodes.map((node, index) => {
     return (
-      <>
+      <React.Fragment key={index}>
         {node}
         {index !== nodes.length - 1 && separator}
-      </>
+      </React.Fragment>
     )
   })
 }
