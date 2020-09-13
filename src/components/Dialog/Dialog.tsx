6463aa8b42fe436bd5bc606b890f319884775183
ShinCurry <shincurryyang@gmail.com>
2020-09-14 00:50:02 +0800

    WAI-ARIA: Dialog & AppDialog

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useMemo } from 'react';
+import React, { useRef, useMemo, useEffect, useState, useCallback } from 'react';
 import useFocusTrap from '@charlietango/use-focus-trap';
 
 import classNames from 'classnames';
@@ -6,6 +6,7 @@ import { UUI } from '../../core/uui';
 import { useClickAway, useLockBodyScroll } from 'react-use';
 import ReactDOM from 'react-dom';
 import ReactHelper from '../../utils/ReactHelper';
+import { KeyCode } from '../../utils/keyboardHelper';
 
 export interface DialogFeatureProps {
   /**
@@ -17,6 +18,14 @@ export interface DialogFeatureProps {
    * Callback invoked when user click outside of content view.
    */
   onClickAway?: () => void;
+  /**
+   * Callback invoked when user close dialog
+   */
+  onClose?: () => void;
+  /**
+   * Whether lock focus on the backdrop element while Dialog is active
+   * @default true
+   */
   focusTrap?: boolean;
   /**
    * Whether the content of Dialog should be rendered inside a `Portal` where appending inside `portalContainer`(if it provided) or `document.body`.
@@ -58,27 +67,53 @@ export const Dialog = UUI.FunctionComponent({
     usePortal: props.usePortal === undefined ? true : props.usePortal,
     portalContainer: props.portalContainer || ReactHelper.document?.body,
     lockBodyScroll: props.lockBodyScroll === undefined ? true : props.lockBodyScroll,
+    focusTrap: props.focusTrap === undefined ? true : props.focusTrap
   }
 
   useLockBodyScroll(props.open && !!finalProps.lockBodyScroll)
 
   const containerRef = useRef<any>(null)
   useClickAway(containerRef, () => {
-    props.open && props.onClickAway && props.onClickAway()
+    if (props.open) {
+      props.onClickAway && props.onClickAway()
+    }
   })
 
-  const ref = useFocusTrap(props.open && !!props.focusTrap)
+  const restoreElement = useRef<HTMLElement | null>(null)
+
+  useEffect(() => {
+    if (props.open) {
+      restoreElement.current = document.activeElement as HTMLElement
+    } else {
+      restoreElement.current?.focus()
+      restoreElement.current = null
+    }
+  }, [props.open])
+
+  const backdropRef = useFocusTrap(props.open && finalProps.focusTrap)
 
   const backdrop = (
     <Backdrop
-      ref={ref}
+      ref={backdropRef}
+      role="dialog"
+      aria-modal={props.open}
       className={classNames({
         'STATE_opened': props.open
       })}
+      onKeyDown={(event) => {
+        switch (event.keyCode) {
+          case KeyCode.Escape:
+            if (props.open) {
+              props.onClose && props.onClose()
+            }
+            break
+          default:
+            // do nothing
+        }
+      }}
     >
       <Container
         ref={containerRef}
-        role="dialog"
         className={classNames({
           'STATE_opened': props.open
         })}
@@ -93,7 +128,7 @@ export const Dialog = UUI.FunctionComponent({
     : <Portal>{backdrop}</Portal>
   }, [backdrop, finalProps.portalContainer, finalProps.usePortal]);
   return (
-    <Root role="dialog">
+    <Root>
       {wrappedBackdrop}
     </Root>
   )
