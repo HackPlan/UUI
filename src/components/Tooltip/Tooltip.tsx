45de0b34d9c4f9c6f1975fef509012dd80143eaa
ShinCurry <shincurryyang@gmail.com>
2021-01-14 06:08:11 +0800

    update Tooltip

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -3,31 +3,39 @@ import React, { useState } from 'react';
 import { usePopper } from 'react-popper';
 import { UUIFunctionComponent, UUIFunctionComponentProps } from '../../core';
 import { createComponentPropTypes, PropTypes } from '../../utils/createPropTypes';
-import { PopoverPlacementPropTypes } from '../Popover';
+import { PopoverPlacementPropTypes, PopoverStrategyPropTypes } from '../Popover';
 
 export type TooltipPlacement = Exclude<Placement, ''>
+export type TooltipStrategy = 'absolute' | 'fixed'
 
 export interface TooltipFeatureProps {
   /**
    * The content of Tooltip
    */
-  label: string;
+  label: React.ReactNode;
   /**
    * The activator elements of Tooltip
    */
   children: React.ReactNode;
   /**
-   * The position (relative to the activator) at which the popover should appear.
+   * The position (relative to the activator) at which the Tooltip should appear.
    * @default 'bottom'
    */
   placement?: TooltipPlacement;
+  /**
+   * The position strategy
+   * @default absolute
+   */
+  strategy?: TooltipStrategy;
 }
 
 export const TooltipPlacementPropTypes = PopoverPlacementPropTypes
+export const TooltipStrategyPropTypes = PopoverStrategyPropTypes
 export const TooltipPropTypes = createComponentPropTypes<TooltipFeatureProps>({
-  label: PropTypes.string.isRequired,
+  label: PropTypes.node.isRequired,
   children: PropTypes.node.isRequired,
   placement: TooltipPlacementPropTypes,
+  strategy: TooltipStrategyPropTypes,
 })
 
 export const Tooltip = UUIFunctionComponent({
@@ -46,12 +54,14 @@ export const Tooltip = UUIFunctionComponent({
    */
   const finalProps = {
     placement: props.placement || 'bottom',
+    strategy: props.strategy || 'fixed',
   }
 
   const [referenceElement, setReferenceElement] = useState<any>(null);
   const [popperElement, setPopperElement] = useState<any>(null);
   const { styles, attributes } = usePopper(referenceElement, popperElement, {
     placement: finalProps.placement,
+    strategy: finalProps.strategy,
     modifiers: [
       { name: 'offset', options: { offset: [0, 8] } },
     ]
@@ -66,7 +76,7 @@ export const Tooltip = UUIFunctionComponent({
         style={{...styles.popper}} {...attributes.popper}
       >
         {props.label}
-        <Arrow data-popper-arrow></Arrow>
+        <Arrow style={{...styles.arrow}} data-popper-arrow></Arrow>
       </Tip>
     </Root>
   )
